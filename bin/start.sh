11b5e1f4502d86dd03f289d4d490ddd62c2510a4
AlexYurshin <102825417+AlexYurshin@users.noreply.github.com>
2022-04-03 20:59:26 +0300

    Composer update + fix compose.yaml (#3)

--- bin/start.sh
+++ bin/start.sh
@@ -6,13 +6,6 @@ docker-compose -f docker-compose.yaml build
 echo "Up project..."
 docker-compose -f docker-compose.yaml up -d --force-recreate
 
-echo "Waiting Elasticsearch..."
-until curl -X GET http://localhost:9200  &> /dev/null
-do
-    echo "Waiting ..."
-    sleep 2
-done
-
 echo "Start"
 docker exec -it app bash
 
